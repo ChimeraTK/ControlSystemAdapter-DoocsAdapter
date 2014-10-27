e81e536136be25af182fa2845681c9b444818bdb
janwych <janwych@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2014-10-27 10:03:39 +0000

    test fixture vals init fix
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@4 688d77b5-b833-4f6b-b27f-eb88d88625d6

--- DOOCS_Adapter/tests/src/testDOOCSProcessVariableAdapter.cpp
+++ DOOCS_Adapter/tests/src/testDOOCSProcessVariableAdapter.cpp
@@ -8,12 +8,8 @@ using namespace boost::unit_test;
 
 
 
-	DOOCSPVAdapter doocs_adapter;
-    
-    myD_int        mydint( NULL, NULL, & doocs_adapter );
 
-
-struct TestStuffFixture {
+struct DPVATestFixture {
 	
 	unsigned int _get_cb_counter;
 	unsigned int _set_cb_counter;
@@ -22,15 +18,12 @@ struct TestStuffFixture {
 	int _cbTestVar;		// callback functionality test variable
 
     DOOCSPVAdapter * doocs_adapter;
-    myD_int        * mydint;
 
-	TestStuffFixture() : _get_cb_counter(0), _set_cb_counter(0), _cbTestVar(0) {
+	DPVATestFixture() : _get_cb_counter(0), _set_cb_counter(0), _cbTestVar(0), _set_cb_counter_equals(0) {
         doocs_adapter = new DOOCSPVAdapter();
-        mydint = new myD_int( NULL, NULL, doocs_adapter );
     }
-	~TestStuffFixture() {
+	~DPVATestFixture() {
         delete doocs_adapter;
-        delete mydint;
     }
 	
 	void	reset_fixture()
@@ -53,18 +46,16 @@ struct TestStuffFixture {
 				_cbTestVar = newValue;
 				++_set_cb_counter;
 			}
-
-
 };
 
 
 
 
-BOOST_FIXTURE_TEST_SUITE( test_callbacks, TestStuffFixture )
+BOOST_FIXTURE_TEST_SUITE( test_callbacks, DPVATestFixture )
 
 BOOST_AUTO_TEST_CASE( test_get )
 {
-	doocs_adapter->setOnGetCallbackFunction(boost::bind (&TestStuffFixture::on_get_callback, this));
+	doocs_adapter->setOnGetCallbackFunction(boost::bind (&DPVATestFixture::on_get_callback, this));
 	
 	BOOST_CHECK( _cbTestVar      == 0 );
 	BOOST_CHECK( _get_cb_counter == 0 );
@@ -100,7 +91,7 @@ BOOST_AUTO_TEST_CASE( test_get )
 
 BOOST_AUTO_TEST_CASE( test_set )
 {
-	doocs_adapter->setOnSetCallbackFunction(boost::bind (&TestStuffFixture::on_set_callback, this, _1, _2));
+	doocs_adapter->setOnSetCallbackFunction(boost::bind (&DPVATestFixture::on_set_callback, this, _1, _2));
 	
 	BOOST_CHECK( _cbTestVar      == 0 );
 	BOOST_CHECK( _set_cb_counter == 0 );
@@ -143,7 +134,7 @@ BOOST_AUTO_TEST_SUITE_END()
 
 
 
-BOOST_FIXTURE_TEST_SUITE( test_no_callbacks, TestStuffFixture )
+BOOST_FIXTURE_TEST_SUITE( test_no_callbacks, DPVATestFixture )
 
 BOOST_AUTO_TEST_CASE( test_setwithoutcallback_getwithoutcallback )
 {
@@ -207,7 +198,31 @@ BOOST_AUTO_TEST_SUITE_END()
 
 
 
-BOOST_FIXTURE_TEST_SUITE( test_sync, TestStuffFixture )
+// ============================================================================
+
+
+struct CbSyncTestFixture {
+	
+    DOOCSPVAdapter * doocs_adapter;
+    myD_int        * mydint;
+
+
+	CbSyncTestFixture() {
+        doocs_adapter = new DOOCSPVAdapter();
+        mydint        = new myD_int( NULL, NULL, doocs_adapter );
+    }
+    
+	~CbSyncTestFixture() {
+        delete doocs_adapter;
+        delete mydint;
+    }
+
+};
+
+
+
+
+BOOST_FIXTURE_TEST_SUITE( test_sync, CbSyncTestFixture )
 
 BOOST_AUTO_TEST_CASE( test_sync1 )
 {
@@ -234,7 +249,6 @@ BOOST_AUTO_TEST_CASE( test_sync1 )
 	
 
 	doocs_adapter->set(0);
-	reset_fixture();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
