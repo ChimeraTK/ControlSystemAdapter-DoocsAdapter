a8ba9b30e04defde9399fc82c0b119616651b6e1
janwych <janwych@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2014-11-06 00:23:43 +0000

    moved D_int_mock.hpp to tests
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@14 688d77b5-b833-4f6b-b27f-eb88d88625d6

---
+++ DOOCS_Adapter/tests/include/D_int_mock.hpp
@@ -0,0 +1,51 @@
+#ifndef __d_int_mock__
+#define __d_int_mock__
+
+
+
+
+
+class EqFct {}; // stub
+
+
+/**
+ * simplified DOOCS property model for tests
+ * http://ttfinfo.desy.de/doocs/doocs_libs/serverlib/html/d__fct_8h_source.html  -> class D_int
+ * http://doocs.desy.de/cgi-bin/viewvc.cgi/source/serverlib/d_fct.cc?view=markup -> D_int::set_value
+ */
+class D_int
+{
+
+protected:
+	int				value_;
+	
+public:
+    
+    unsigned int __set_value_f_call_counter;                // __
+    unsigned int __value_f_call_counter;                    // __
+
+    D_int (const char *pn, EqFct *ef) : value_(0), __set_value_f_call_counter(0), __value_f_call_counter(0) {}
+    D_int (const char *pn)            : value_(0), __set_value_f_call_counter(0), __value_f_call_counter(0) {}
+
+
+	virtual void	set_value (int val)
+					{
+                        __set_value_f_call_counter++;       // __
+						value_ = val;
+					}
+
+	virtual int		value ()
+					{
+                        __value_f_call_counter++;           // __
+						return value_;
+					}
+};
+
+
+
+
+
+
+
+#endif /* __d_int_mock__ */
+
