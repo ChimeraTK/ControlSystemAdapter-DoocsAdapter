83fe7bf67e1153f7cf1b0543d3e2d72b7d114c5a
janwych <janwych@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2015-01-29 13:27:55 +0000

    added callbacks-enabled float-based prototype D_spectrum derivative
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@51 688d77b5-b833-4f6b-b27f-eb88d88625d6

---
+++ DOOCS_Adapter/include/m4uD_array.hpp
@@ -0,0 +1,112 @@
+#ifndef __m4uD_array__
+#define __m4uD_array__
+
+
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+
+
+
+namespace mtca4u {
+
+
+
+
+//~ template <typename T, typename DOOCS_T>
+class m4uD_array : public D_spectrum
+{
+
+protected:
+    boost::function< void (ProcessArray<float> const & ) > _onSetCallbackFunction;  // newValue
+    boost::function< void (ProcessArray<float> & ) >       _onGetCallbackFunction;    // toBeFilled
+
+    
+public:
+
+
+    void setOnSetCallbackFunction (boost::function< void (ProcessArray<float> const & ) > onSetCallbackFunction)
+         {
+             _onSetCallbackFunction = onSetCallbackFunction;
+         }
+
+    void setOnGetCallbackFunction (boost::function< void (ProcessArray<float> & ) > onGetCallbackFunction)
+         {
+             _onGetCallbackFunction = onGetCallbackFunction;
+         }
+
+    void clearOnSetCallbackFunction ()
+         {
+             _onSetCallbackFunction.clear ();
+         }
+
+    void clearOnGetCallbackFunction ()
+         {
+             _onGetCallbackFunction.clear ();
+         }
+
+
+
+
+    m4uD_array (const char *pn, int maxl, EqFct *ef) :    D_spectrum(pn, maxl, ef) {}
+
+
+
+
+    // foundation for random access, based on direct mapping on D_spectrum interface
+    void  fill_spectrum (int i, float data)
+          {
+              D_spectrum::fill_spectrum (i, data);
+          }
+    float read_spectrum (int i) const
+          {
+              return D_spectrum::read_spectrum (i);
+          }
+
+
+    // treat whole spectrum at once - for set and get, extending D_spectrum interface
+    // // with callbacks
+    void  fill_whole_spectrum (const std::vector<float> & data)
+          {
+              fill_whole_spectrum_without_callback(data);
+              if (_onSetCallbackFunction){
+                  StubProcessArray<float> pa(5); _onSetCallbackFunction( pa );        // FIXME: here rather _onSetCallbackFunction(*this); or sth instead of the whole line
+              }
+          }
+    std::vector<float> read_whole_spectrum () const        // FIXME: a copy return
+          {
+              if (_onGetCallbackFunction){
+                  StubProcessArray<float> pa(5); _onGetCallbackFunction( pa );        // FIXME: here rather _onGetCallbackFunction(*this); or sth instead of the whole line
+              }
+              return read_whole_spectrum_without_callback();        // FIXME: make sure returned is what comes from callback
+          }
+
+    // // without callbacks
+    void  fill_whole_spectrum_without_callback (const std::vector<float> & data)
+          {
+              for (size_t i=0; i<data.size(); ++i)  // size agreement checked by the adapter
+              {
+                  D_spectrum::fill_spectrum (i, data[i]);
+              }
+          }
+    std::vector<float> read_whole_spectrum_without_callback () const        // FIXME: a copy return
+          {
+              std::vector<float> v;
+              for (int i=0; i<max_length_; ++i)  // size agreement checked by the adapter
+              {
+                  v.push_back(D_spectrum::read_spectrum(i));
+              }
+              return v;
+          }
+
+};
+
+
+
+
+
+}//namespace mtca4u
+
+
+#endif /* __m4uD_array__ */
+
---
+++ DOOCS_Adapter/tests/src/testm4uD_array_float.cpp
@@ -0,0 +1,383 @@
+#define BOOST_TEST_MODULE test_array_float
+
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+using namespace boost::unit_test;
+
+
+
+#include "eq_fct.h"
+#include "ProcessArray.h"
+#include "StubProcessArray.h"       //FIXME - shouldn't be here in the end
+#include "m4uD_array.hpp"
+
+
+
+
+// ============================================================================
+
+
+struct CallbacksTestFixture {
+    
+    //~ mtca4u::m4uD_type<float, D_float>   mydtype;
+    mtca4u::m4uD_array                  mydarray;
+
+
+    unsigned int _get_cb_counter;
+    unsigned int _set_cb_counter;
+
+    std::vector<float> vec_test;
+    std::vector<float> vec_mydarray;
+
+
+            CallbacksTestFixture() : mydarray( NULL, 4, NULL ),
+                                     _get_cb_counter       (0),
+                                     _set_cb_counter       (0),
+                                     vec_test(4)
+            {}
+    
+            ~CallbacksTestFixture() {}
+    
+        
+
+    //~ void    reset_counters()
+            //~ {
+                //~ _get_cb_counter        = 0;
+                //~ _set_cb_counter        = 0;
+            //~ }
+    
+
+            // callbacks
+    void    on_set_callback (mtca4u::ProcessArray<float> const & ) //~  < void (ProcessArray<float> const & ) >
+            {
+                ++_set_cb_counter;
+                mydarray.fill_spectrum (0, 5.0);
+            }
+    void    on_get_callback (mtca4u::ProcessArray<float> & )       //~  < void (ProcessArray<float> & ) >
+            {
+                ++_get_cb_counter;
+                mydarray.fill_spectrum (3, 6.0);
+            }
+};
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+BOOST_FIXTURE_TEST_SUITE( test_operation, CallbacksTestFixture ) // operation check: return values matter
+
+	BOOST_AUTO_TEST_CASE( test_getset_nocb )
+	{
+        // vec_test = [0,0,0,0]
+        vec_test.assign(4, 0.0);
+
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+        mydarray.fill_whole_spectrum(vec_test);
+        
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+	}
+
+
+	BOOST_AUTO_TEST_CASE( test_getwcsetwc_nocb )
+	{
+        // vec_test = [0,0,0,0]
+        vec_test.assign(4, 0.0);
+
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+        
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+	}
+
+
+	BOOST_AUTO_TEST_CASE( test_randomaccess )
+	{
+        mydarray.fill_spectrum (0, 1.0);
+        mydarray.fill_spectrum (1, 2.0);
+        mydarray.fill_spectrum (2, 3.0);
+        mydarray.fill_spectrum (3, 4.0);
+        
+        BOOST_CHECK_EQUAL(mydarray.read_spectrum(0), 1.0);
+        BOOST_CHECK_EQUAL(mydarray.read_spectrum(1), 2.0);
+        BOOST_CHECK_EQUAL(mydarray.read_spectrum(2), 3.0);
+        BOOST_CHECK_EQUAL(mydarray.read_spectrum(3), 4.0);
+    }
+
+
+	BOOST_AUTO_TEST_CASE( test_getset_cb )
+	{
+        // vec_test = [0,0,0,0]
+        vec_test.assign(4, 0.0);
+
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+        mydarray.fill_whole_spectrum(vec_test);
+        
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+
+        // vec_test = [2,2,2,2], then [5,2,2,2]
+        vec_test.assign(4, 2.0);
+        mydarray.fill_whole_spectrum(vec_test);
+                                                                                 vec_test[0] = 5;// vec_test[3] = 6; 
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+
+        // vec_test = [3,3,3,3], then [5,3,3,6]
+        vec_test.assign(4, 3.0);
+        mydarray.fill_whole_spectrum(vec_test);
+                                                                                 vec_test[0] = 5; vec_test[3] = 6; 
+        vec_mydarray = mydarray.read_whole_spectrum();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+	}
+
+
+	BOOST_AUTO_TEST_CASE( test_getwcsetwc_cb )
+	{
+        // vec_test = [0,0,0,0]
+        vec_test.assign(4, 0.0);
+
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+        
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+
+        // vec_test = [2,2,2,2]
+        vec_test.assign(4, 2.0);
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+                                                                                // vec_test[0] = 5;// vec_test[3] = 6; 
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+
+        // vec_test = [3,3,3,3]
+        vec_test.assign(4, 3.0);
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+                                                                                // vec_test[0] = 5; vec_test[3] = 6; 
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+        BOOST_CHECK_EQUAL_COLLECTIONS(vec_mydarray.begin(), vec_mydarray.end(), vec_test.begin(), vec_test.end());
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+// ============================================================================
+
+
+BOOST_FIXTURE_TEST_SUITE( test_callbacks, CallbacksTestFixture ) // callback arbitrarily present: callback counters matter, return values don't
+
+	BOOST_AUTO_TEST_CASE( test_get_cb_count )
+	{
+		BOOST_CHECK( _get_cb_counter        == 0 );
+		
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+		
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+		
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 1 );
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 2 );
+
+
+		mydarray.clearOnGetCallbackFunction();
+		
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 2 );
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 2 );
+
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+		
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 3 );
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 4 );
+
+
+		mydarray.clearOnGetCallbackFunction();
+		
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 4 );
+        vec_mydarray = mydarray.read_whole_spectrum();
+		BOOST_CHECK( _get_cb_counter        == 4 );
+	}
+
+
+	BOOST_AUTO_TEST_CASE( test_set_cb_count )
+	{
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+
+		BOOST_CHECK( _set_cb_counter        == 0 );
+		
+        mydarray.fill_whole_spectrum(vec_test);
+        BOOST_CHECK( _set_cb_counter        == 0 );
+
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+		
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+		
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 1 );
+
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 2 );
+
+
+		mydarray.clearOnSetCallbackFunction();
+		
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 2 );
+
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 2 );
+
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+		
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 3 );
+
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 4 );
+
+
+		mydarray.clearOnSetCallbackFunction();
+		
+        mydarray.fill_whole_spectrum(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 4 );
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+// ============================================================================
+
+
+BOOST_FIXTURE_TEST_SUITE( test_no_callbacks, CallbacksTestFixture ) // callback absent: callback counters matter, return values don't
+
+	BOOST_AUTO_TEST_CASE( test_get_nocb_count )
+	{
+		BOOST_CHECK( _get_cb_counter        == 0 );
+		
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+		
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+		
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+
+
+		mydarray.clearOnGetCallbackFunction();
+		
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+
+
+		mydarray.setOnGetCallbackFunction(boost::bind (&CallbacksTestFixture::on_get_callback, this, _1));
+		
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+
+
+		mydarray.clearOnGetCallbackFunction();
+		
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+        vec_mydarray = mydarray.read_whole_spectrum_without_callback();
+		BOOST_CHECK( _get_cb_counter        == 0 );
+	}
+
+
+	BOOST_AUTO_TEST_CASE( test_set_nocb_count )
+	{
+        // vec_test = [1,1,1,1]
+        vec_test.assign(4, 1.0);
+
+		BOOST_CHECK( _set_cb_counter        == 0 );
+		
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+        BOOST_CHECK( _set_cb_counter        == 0 );
+
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+		
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+		
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+
+		mydarray.clearOnSetCallbackFunction();
+		
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+
+		mydarray.setOnSetCallbackFunction(boost::bind (&CallbacksTestFixture::on_set_callback, this, _1));
+		
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+
+
+		mydarray.clearOnSetCallbackFunction();
+		
+        mydarray.fill_whole_spectrum_without_callback(vec_test);
+		BOOST_CHECK( _set_cb_counter        == 0 );
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
