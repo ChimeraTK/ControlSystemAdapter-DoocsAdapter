5c001b23f816fc29e430e7dc2a1bc6e4c504730f
killenb <killenb@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2015-09-30 15:47:20 +0000

    finished test for IndepedentControlCore (no doocs involved)
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/branches/redesign_sebastian_noMock@112 688d77b5-b833-4f6b-b27f-eb88d88625d6

--- DOOCS_Adapter/tests/src/testIndependentControlCore.cpp
+++ DOOCS_Adapter/tests/src/testIndependentControlCore.cpp
@@ -35,6 +35,8 @@ BOOST_AUTO_TEST_CASE( independentControlCoreTest ){
   // the other side has a timeout of 100 ms, so we wait 1 second to be safe,
   // but we look every 10 ms (not too frequent, but only with 10 % of additional latency)
   //syncUtil.waitForNotifications(1000000, 10000);
+  //Note: as waitForNotification does not work as expected, the functionality is
+  // done manually for just the one variable we are receiving.
   for (size_t i=0; i<  100; ++i){
     boost::this_thread::sleep_for( boost::chrono::milliseconds(10) );
     if (monitorVoltage->receive()){
@@ -46,8 +48,26 @@ BOOST_AUTO_TEST_CASE( independentControlCoreTest ){
   BOOST_CHECK(  *targetVoltage == -1 );
   // the monitor voltage has to be 0 now, like in the hardware
   BOOST_CHECK(  *monitorVoltage == 0 );
-  //syncUtil.waitForNotifications(1000000, 10000);
 
+  *targetVoltage = 42;
+  targetVoltage->send();
+
+  // Wait until we read something back and check that the expected value is there.
+  // This is not nevessarily the case on the first read, so we put a limit to 10.
+  // Note that the number of actual receive attempty is higher, because we 
+  // read every 10 ms while the other side is sending every 100 ms.
+  for (size_t receiveCounter=0; receiveCounter<  10; /*empty*/){
+    boost::this_thread::sleep_for( boost::chrono::milliseconds(10) );
+    if (monitorVoltage->receive()){
+      ++receiveCounter;
+      if (*monitorVoltage == 42){
+	break;
+      }
+    }
+  }
+
+  BOOST_CHECK(*monitorVoltage == 42);
+  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
