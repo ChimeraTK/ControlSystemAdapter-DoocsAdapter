48080d2efd8ec37b0f48dbadf82b88e4721ac94a
killenb <killenb@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2015-10-01 19:54:17 +0000

    refactored the example to separate the EqFct from the location-independent server (main) functions
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/branches/redesign_sebastian_noMock@120 688d77b5-b833-4f6b-b27f-eb88d88625d6

--- DOOCS_Adapter/example/CosadeEqFct.cc
+++ DOOCS_Adapter/example/CosadeEqFct.cc
@@ -1,6 +1,4 @@
-#include "eq_cosade.h"
-
-const char * object_name = "Cosade server";
+#include "CosadeEqFct.h"
 
 CosadeEqFct::CosadeEqFct ( ) : EqFct ("NAME = cosade" ), doocsAdapter_(this){
   
@@ -24,26 +22,3 @@ void CosadeEqFct::update(){
   // variables, so Doocs knows the current values.
   doocsAdapter_.receiveAll();
 }
-
-// The usual function to create locations. Present in every doocs server.
-EqFct * eq_create (int eq_code, void *){
-   switch (eq_code) {
-      case CODE_COSADE:
-	 return  new CosadeEqFct ();
-      default:
-	 return (EqFct *) 0;
-   }
-}
-
-// all the bloat we have to implement for DOOCS although we don't need it
-void eq_init_prolog() {}
-void eq_init_epilog() {}
-void refresh_prolog() {}
-void refresh_epilog() {}
-void interrupt_usr1_prolog(int) {}
-void interrupt_usr2_prolog(void) {}
-void interrupt_usr1_epilog(int) {}
-void interrupt_usr2_epilog(void) {}
-void post_init_prolog(void) {}
-void post_init_epilog(void) {}
-void eq_cancel(void) {}
