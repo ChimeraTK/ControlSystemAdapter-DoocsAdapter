251157f70829c409581a9d5e4d0c029ffe107ae2
killenb <killenb@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2016-03-17 07:59:41 +0000

    moved empty doocs functions into the adapter. Cosade server is down to 5 lines total.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@137 688d77b5-b833-4f6b-b27f-eb88d88625d6

--- DOOCS_Adapter/example/cosade_server.cc
+++ DOOCS_Adapter/example/cosade_server.cc
@@ -1,22 +1,10 @@
-#include "IndependentControlCore.h"
 #include <DoocsAdapter.h>
 
-#include <iostream>
+// Include all the control applications you want in this server
+#include "IndependentControlCore.h"
 
 BEGIN_DOOCS_SERVER("Cosade server", 10)
+   // Create static instances for all applications cores. They must not have overlapping
+   // process variable names ("location/protery" must be unique).
    static IndependentControlCore independentControlCore(doocsAdapter.getDevicePVManager());
-   std::cout << "this is eq_create" << std::endl;
 END_DOOCS_SERVER()
-
-// all the bloat we have to implement for DOOCS although we don't need it
-void eq_init_prolog() {std::cout << "this is eq_init_prolog" << std::endl;}
-void eq_init_epilog() {std::cout << "this is eq_init_epilog" << std::endl;}
-void refresh_prolog() {}
-void refresh_epilog() {}
-void interrupt_usr1_prolog(int) {}
-void interrupt_usr2_prolog(void) {}
-void interrupt_usr1_epilog(int) {}
-void interrupt_usr2_epilog(void) {}
-void post_init_prolog(void) {}
-void post_init_epilog(void) {}
-void eq_cancel(void) {}
