5f684b012c0ee9087a4c9f596fb53e977d6dd3de
killenb <killenb@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2016-05-13 15:51:33 +0000

    - added c++11 flags for compiler - added function to split a string at the first slash
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@140 688d77b5-b833-4f6b-b27f-eb88d88625d6

---
+++ DOOCS_Adapter/tests/src/testSplitStringAtFirstSlash.cpp
@@ -0,0 +1,36 @@
+// Define a name for the test module.
+#define BOOST_TEST_MODULE SplitStringAtFirstSlashTest
+// Only after defining the name include the unit test header.
+#include <boost/test/included/unit_test.hpp>
+
+#include "splitStringAtFirstSlash.h"
+
+using namespace boost::unit_test_framework;
+using namespace mtca4u;
+
+BOOST_AUTO_TEST_SUITE( SplitTestSuite )
+
+BOOST_AUTO_TEST_CASE( testSpitting ) {
+  auto splitResult = splitStringAtFirstSlash("stringWithout.a.slash");
+  BOOST_CHECK( splitResult.first == "" );
+  BOOST_CHECK( splitResult.second == "stringWithout.a.slash" );
+
+  splitResult = splitStringAtFirstSlash("/stringStartingWith.slash");
+  BOOST_CHECK( splitResult.first == "" );
+  BOOST_CHECK( splitResult.second == "stringStartingWith.slash" );
+
+  splitResult = splitStringAtFirstSlash("stringEndingWith.slash/");
+  BOOST_CHECK( splitResult.first == "stringEndingWith.slash" );
+  BOOST_CHECK( splitResult.second == "" );
+
+  splitResult = splitStringAtFirstSlash("string/With.slash");
+  BOOST_CHECK( splitResult.first == "string" );
+  BOOST_CHECK( splitResult.second == "With.slash" );
+
+  splitResult = splitStringAtFirstSlash("string/With/twoSlashes");
+  BOOST_CHECK( splitResult.first == "string" );
+  BOOST_CHECK( splitResult.second == "With/twoSlashes" );
+  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
