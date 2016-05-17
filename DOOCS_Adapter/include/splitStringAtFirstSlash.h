fba675c856cee0f07f3c53954bd17c010239c47d
killenb <killenb@688d77b5-b833-4f6b-b27f-eb88d88625d6>
2016-05-17 08:54:26 +0000

    added missig header with helper function
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u_applications/DOOCS_Adapter/trunk@142 688d77b5-b833-4f6b-b27f-eb88d88625d6

---
+++ DOOCS_Adapter/include/splitStringAtFirstSlash.h
@@ -0,0 +1,17 @@
+#ifndef _DOOCS_ADAPTER_SPLIT_STRING_AT_FIRST_SLASH_
+#define _DOOCS_ADAPTER_SPLIT_STRING_AT_FIRST_SLASH_
+
+namespace mtca4u{
+
+  std::pair< std::string, std::string > splitStringAtFirstSlash(std::string input){
+    auto slashPosition = input.find_first_of("/");
+    if (slashPosition == std::string::npos){
+      return std::make_pair( std::string(), input);
+    }
+    return std::make_pair( input.substr( 0, slashPosition) ,
+			   input.substr( slashPosition+1 ) );
+  }
+
+}
+
+#endif // _DOOCS_ADAPTER_SPLIT_STRING_AT_FIRST_SLASH_
