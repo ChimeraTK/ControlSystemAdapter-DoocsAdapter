cb2a99db9efcbfb1933e326936dc4f8f26fe8e9e
Martin Killenberg <martin.killenberg@desy.de>
2017-08-08 11:53:29 +0200

    parsing xml file successfully. Not interpreted yet.

--- src/VariableMapper.cc
+++ src/VariableMapper.cc
@@ -1,12 +1,19 @@
 #include "VariableMapper.h"
 
+#include <libxml++/libxml++.h>
+
 namespace ChimeraTK{
   VariableMapper & VariableMapper::getInstance(){
     static VariableMapper instance;
     return instance;
   }
 
-  
+  void VariableMapper::prepareOutput(std::string xmlFile, std::set< std::string > inputVariables){
+    xmlpp::DomParser parser;
+    //    parser.set_validate();
+    parser.set_substitute_entities(); //We just want the text to be resolved/unescaped automatically.
+    parser.parse_file(xmlFile);
+  }
 
 } // namespace ChimeraTK
 
--- tests/src/testVariableMapper.cpp
+++ tests/src/testVariableMapper.cpp
@@ -11,3 +11,57 @@ BOOST_AUTO_TEST_CASE( testCreation ){
   VariableMapper & vm2 = VariableMapper::getInstance();
   BOOST_CHECK( &vm == &vm2 );
 }
+
+/** Here is the example tree on which the examples and tests are based:
+ *\verbatim
+/
+├── A
+│   ├── a
+│   │   ├── di
+│   │   └── do
+│   └── b
+├── B
+│   ├── a
+│   │   └── dr
+│   └── c
+│       ├── de
+│       └── gne
+├── C
+│   ├── a
+│   │   └── da
+│   ├── b
+│   │   └── ge
+│   └── c
+│       ├── be
+│       └── de
+└── DIRECT
+    ├── DOUBLE
+    ├── DOUBLE_ARRAY
+    ├── INT
+    └── INT_ARRAY
+ *\endverbatim
+ */
+std::set< std::string > generateInputVariables(){
+  std::set< std::string > inputVariables;
+  inputVariables.insert("/A/a/di");
+  inputVariables.insert("/A/a/do");
+  inputVariables.insert("/A/b");
+  inputVariables.insert("/B/a/dr");
+  inputVariables.insert("/B/c/de");
+  inputVariables.insert("/B/c/gne");
+  inputVariables.insert("/C/a/da");  
+  inputVariables.insert("/C/b/ge");  
+  inputVariables.insert("/C/c/be");  
+  inputVariables.insert("/C/c/de"); // there also is a c/de in B
+  inputVariables.insert("/DIRECT/DOUBLE");  
+  inputVariables.insert("/DIRECT/DOUBLE_ARRAY");  
+  inputVariables.insert("/DIRECT/INT");  
+  inputVariables.insert("/DIRECT/INT_ARRAY");
+
+  return inputVariables;
+}
+
+BOOST_AUTO_TEST_CASE( testRename ){
+  VariableMapper & vm = VariableMapper::getInstance();
+  vm.prepareOutput("variableTreeXml/rename.xml", generateInputVariables());
+}
