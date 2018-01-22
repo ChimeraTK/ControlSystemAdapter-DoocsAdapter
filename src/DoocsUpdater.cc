6c1f3cfa50ac28de50912822069baa9c25f009d8
Martin Hierholzer <martin.hierholzer@desy.de>
2018-01-22 13:03:14 +0100

    increased sleep for update loop to 50ms to fix high cpu loads

--- src/DoocsUpdater.cc
+++ src/DoocsUpdater.cc
@@ -36,7 +36,7 @@ namespace ChimeraTK{
       */
       /// @todo FIXME this is a temporary (?) work-around for the slow readAny()
       update();
-      boost::this_thread::sleep_for(boost::chrono::milliseconds(10));
+      boost::this_thread::sleep_for(boost::chrono::milliseconds(50));
     }
   }
 
