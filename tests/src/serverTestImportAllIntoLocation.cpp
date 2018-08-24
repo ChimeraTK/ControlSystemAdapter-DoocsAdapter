e72b7feeaae818aa946e75ae8e54f88e27782423
Martin Killenberg <martin.killenberg@desy.de>
2018-08-24 17:57:37 +0200

    added debug output and changed order of calls in a test. Still failing with DOOCS head

--- tests/src/serverTestImportAllIntoLocation.cpp
+++ tests/src/serverTestImportAllIntoLocation.cpp
@@ -28,14 +28,18 @@ using namespace ChimeraTK;
 
 /// Check that all expected variables are there.
 void testVariableExistence(){
-  for (auto const directory : { "CHAR", "DOUBLE", "FLOAT", "INT", "SHORT", "UCHAR", "UINT", "USHORT"} ){
+  //  for (auto const directory : { "CHAR", "DOUBLE", "FLOAT", "INT", "SHORT", "UCHAR", "UINT", "USHORT"} ){
+  for (auto const directory : { "DOUBLE", "FLOAT", "INT", "SHORT", "UCHAR", "UINT", "USHORT", "CHAR"} ){
     for (auto const variable : { "CONSTANT_ARRAY", "FROM_DEVICE_ARRAY", "TO_DEVICE_ARRAY "} ){
       // if this throws the property does not exist. we should always be able to read"
+      std::cout << "testing existence of " << std::string("//MASTER/")+directory+"."+ variable << std::endl;
       BOOST_CHECK_NO_THROW( DoocsServerTestHelper::doocsGetArray<int>( (std::string("//MASTER/")+directory+"."+ variable).c_str() ));
     }
     for (auto const variable : { "DATA_TYPE_CONSTANT", "FROM_DEVICE_SCALAR", "TO_DEVICE_SCALAR"} ){
+      std::cout << "testing existence of " << std::string("//MASTER/")+directory+"."+ variable << std::endl;
       // if this throws the property does not exist. we should always be able to read"
       BOOST_CHECK_NO_THROW( DoocsServerTestHelper::doocsGet<int>( (std::string("//MASTER/")+directory+"."+ variable).c_str() ));
+      std::cout << "test done " << std::endl;
     }
   }
 }
