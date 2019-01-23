42bd631f588f09b8e1d50c963597f9f77684983b
Jens Georg <jens.georg@desy.de>
2019-01-23 11:18:13 +0100

    Interrupt all read variables
    Fixes DOOCS server shutdown lock-up

--- src/DoocsUpdater.cc
+++ src/DoocsUpdater.cc
@@ -47,6 +47,9 @@ namespace ChimeraTK{
 
   void DoocsUpdater::stop(){
     _syncThread.interrupt();
+    for (auto & var: _elementsToRead) {
+        var.getHighLevelImplElement()->interrupt();
+    }
     _syncThread.join();
   }
 
