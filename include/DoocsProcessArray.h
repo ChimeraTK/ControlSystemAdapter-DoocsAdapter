156f760f09e2774425d1cd999b025d6c8f060fbc
Martin Hierholzer <martin.hierholzer@desy.de>
2019-02-22 12:52:59 +0100

    added missing header files

--- include/DoocsProcessArray.h
+++ include/DoocsProcessArray.h
@@ -8,132 +8,112 @@
 #include <ChimeraTK/ControlSystemAdapter/ProcessVariableListener.h>
 #include <ChimeraTK/OneDRegisterAccessor.h>
 
+#include "DoocsUpdater.h"
 #include "splitStringAtFirstSlash.h"
 
 #include <eq_fct.h>
 
 namespace ChimeraTK {
 
-template <typename DOOCS_T, typename DOOCS_PRIMITIVE_T>
-class DoocsProcessArray : public DOOCS_T, public boost::noncopyable {
-public:
-  DoocsProcessArray(
-      EqFct *eqFct, std::string const &doocsPropertyName,
-      boost::shared_ptr<ChimeraTK::NDRegisterAccessor<DOOCS_PRIMITIVE_T>> const
-          &processArray,
-      DoocsUpdater &updater)
-      : DOOCS_T(doocsPropertyName.c_str(), processArray->getNumberOfSamples(),
-                eqFct),
-        _processArray(processArray) {
-    if (processArray->isReadable()) {
-      updater.addVariable(
-          ChimeraTK::OneDRegisterAccessor<DOOCS_PRIMITIVE_T>(processArray),
-          eqFct,
-          std::bind(
-              &DoocsProcessArray<DOOCS_T, DOOCS_PRIMITIVE_T>::updateDoocsBuffer,
-              this));
+  template<typename DOOCS_T, typename DOOCS_PRIMITIVE_T>
+  class DoocsProcessArray : public DOOCS_T, public boost::noncopyable {
+   public:
+    DoocsProcessArray(EqFct* eqFct, std::string const& doocsPropertyName,
+        boost::shared_ptr<ChimeraTK::NDRegisterAccessor<DOOCS_PRIMITIVE_T>> const& processArray, DoocsUpdater& updater)
+    : DOOCS_T(doocsPropertyName.c_str(), processArray->getNumberOfSamples(), eqFct), _processArray(processArray) {
+      if(processArray->isReadable()) {
+        updater.addVariable(ChimeraTK::OneDRegisterAccessor<DOOCS_PRIMITIVE_T>(processArray),
+            eqFct,
+            std::bind(&DoocsProcessArray<DOOCS_T, DOOCS_PRIMITIVE_T>::updateDoocsBuffer, this));
+      }
+
+      // Check if the array length exceeds the maximum allowed length by DOOCS.
+      // DOOCS does not report this as an error and instead silently truncates the
+      // array length.
+      if(processArray->getNumberOfSamples() != static_cast<size_t>(this->length())) {
+        std::stringstream s;
+        s << "Error: The selected DOOCS data type for the variable '" << processArray->getName() << "' "
+          << "(mapped to the DOOCS name '" << doocsPropertyName << "') seems not to support the requested length of "
+          << processArray->getNumberOfSamples() << " since the DOOCS property has a length of " << this->length()
+          << ". Try selectin a different DOOCS type in the mappng XML file, e.g. "
+             "a D_spectrum!";
+        throw ChimeraTK::logic_error(s.str());
+      }
     }
 
-    // Check if the array length exceeds the maximum allowed length by DOOCS.
-    // DOOCS does not report this as an error and instead silently truncates the
-    // array length.
-    if (processArray->getNumberOfSamples() !=
-        static_cast<size_t>(this->length())) {
-      std::stringstream s;
-      s << "Error: The selected DOOCS data type for the variable '"
-        << processArray->getName() << "' "
-        << "(mapped to the DOOCS name '" << doocsPropertyName
-        << "') seems not to support the requested length of "
-        << processArray->getNumberOfSamples()
-        << " since the DOOCS property has a length of " << this->length()
-        << ". Try selectin a different DOOCS type in the mappng XML file, e.g. "
-           "a D_spectrum!";
-      throw ChimeraTK::logic_error(s.str());
-    }
-  }
-
-  /**
-   * Overload the set function which is called by DOOCS to inject sending to the
-   * device.
-   */
-  void set(EqAdr *eqAdr, EqData *data1, EqData *data2, EqFct *eqFct) override {
-    DOOCS_T::set(eqAdr, data1, data2, eqFct);
-    sendToDevice();
-  }
-
-  /**
-   * Override the Doocs auto_init() method, which is called after initialising
-   * the value of the property from the config file.
-   */
-  void auto_init(void) override {
-    DOOCS_T::auto_init();
-    // send the current value to the device
-    if (_processArray->isWriteable()) {
+    /**
+     * Overload the set function which is called by DOOCS to inject sending to the
+     * device.
+     */
+    void set(EqAdr* eqAdr, EqData* data1, EqData* data2, EqFct* eqFct) override {
+      DOOCS_T::set(eqAdr, data1, data2, eqFct);
       sendToDevice();
     }
-  }
 
-  void updateDoocsBuffer() {
-    // Note: we already own the location lock by specification of the
-    // DoocsUpdater
-    auto &processVector = _processArray->accessChannel(0);
-
-    // We have to cast the pointer to the correct underlying DOOCS type. This
-    // cast never does anything, the only reason is to "convert" from int64_t to
-    // long long int (which are different types!)
-    typedef
-        typename std::result_of<decltype (&DOOCS_T::value)(DOOCS_T, int)>::type
-            THE_DOOCS_TYPE;
-    static_assert(std::is_same<THE_DOOCS_TYPE, DOOCS_PRIMITIVE_T>::value ||
-                      (std::is_same<DOOCS_PRIMITIVE_T, int64_t>::value &&
-                       std::is_same<THE_DOOCS_TYPE, long long int>::value),
-                  "Bad type casting.");
-    auto dataPtr = reinterpret_cast<THE_DOOCS_TYPE *>(processVector.data());
-
-    this->fill_array(dataPtr, processVector.size());
-    if (publishZMQ) {
-      dmsg_info info;
-      memset(&info, 0, sizeof(info));
-      auto sinceEpoch =
-          _processArray->getVersionNumber().getTime().time_since_epoch();
-      auto time =
-          std::chrono::duration_cast<std::chrono::microseconds>(sinceEpoch);
-      info.sec = time.count() / 1000000;
-      info.usec = time.count() % 1000000;
-      info.ident = _macroPulseNumberSource->accessData(0);
-      this->send(&info);
+    /**
+     * Override the Doocs auto_init() method, which is called after initialising
+     * the value of the property from the config file.
+     */
+    void auto_init(void) override {
+      DOOCS_T::auto_init();
+      // send the current value to the device
+      if(_processArray->isWriteable()) {
+        sendToDevice();
+      }
     }
-  }
 
-  void publishZeroMQ() { publishZMQ = true; }
+    void updateDoocsBuffer() {
+      // Note: we already own the location lock by specification of the
+      // DoocsUpdater
+      auto& processVector = _processArray->accessChannel(0);
 
-  void setMacroPulseNumberSource(
-      boost::shared_ptr<ChimeraTK::NDRegisterAccessor<int64_t>>
-          macroPulseNumberSource) {
-    _macroPulseNumberSource = macroPulseNumberSource;
-  }
+      // We have to cast the pointer to the correct underlying DOOCS type. This
+      // cast never does anything, the only reason is to "convert" from int64_t to
+      // long long int (which are different types!)
+      typedef typename std::result_of<decltype (&DOOCS_T::value)(DOOCS_T, int)>::type THE_DOOCS_TYPE;
+      static_assert(std::is_same<THE_DOOCS_TYPE, DOOCS_PRIMITIVE_T>::value ||
+              (std::is_same<DOOCS_PRIMITIVE_T, int64_t>::value && std::is_same<THE_DOOCS_TYPE, long long int>::value),
+          "Bad type casting.");
+      auto dataPtr = reinterpret_cast<THE_DOOCS_TYPE*>(processVector.data());
 
-protected:
-  boost::shared_ptr<ChimeraTK::NDRegisterAccessor<DOOCS_PRIMITIVE_T>>
-      _processArray;
-  boost::shared_ptr<ChimeraTK::NDRegisterAccessor<int64_t>>
-      _macroPulseNumberSource;
-  bool publishZMQ{false};
-
-  // Internal function which copies the content from the DOOCS container into
-  // the ChimeraTK ProcessArray and calls the send method. Factored out to allow
-  // unit testing.
-  void sendToDevice() {
-    // Brute force implementation with a loop. Works for all data types.
-    // always get a fresh reference
-    auto &processVector = _processArray->accessChannel(0);
-    size_t arraySize = processVector.size();
-    for (size_t i = 0; i < arraySize; ++i) {
-      processVector[i] = this->value(i);
+      this->fill_array(dataPtr, processVector.size());
+      if(publishZMQ) {
+        dmsg_info info;
+        memset(&info, 0, sizeof(info));
+        auto sinceEpoch = _processArray->getVersionNumber().getTime().time_since_epoch();
+        auto time = std::chrono::duration_cast<std::chrono::microseconds>(sinceEpoch);
+        info.sec = time.count() / 1000000;
+        info.usec = time.count() % 1000000;
+        info.ident = _macroPulseNumberSource->accessData(0);
+        this->send(&info);
+      }
     }
-    _processArray->write();
-  }
-};
+
+    void publishZeroMQ() { publishZMQ = true; }
+
+    void setMacroPulseNumberSource(boost::shared_ptr<ChimeraTK::NDRegisterAccessor<int64_t>> macroPulseNumberSource) {
+      _macroPulseNumberSource = macroPulseNumberSource;
+    }
+
+   protected:
+    boost::shared_ptr<ChimeraTK::NDRegisterAccessor<DOOCS_PRIMITIVE_T>> _processArray;
+    boost::shared_ptr<ChimeraTK::NDRegisterAccessor<int64_t>> _macroPulseNumberSource;
+    bool publishZMQ{false};
+
+    // Internal function which copies the content from the DOOCS container into
+    // the ChimeraTK ProcessArray and calls the send method. Factored out to allow
+    // unit testing.
+    void sendToDevice() {
+      // Brute force implementation with a loop. Works for all data types.
+      // always get a fresh reference
+      auto& processVector = _processArray->accessChannel(0);
+      size_t arraySize = processVector.size();
+      for(size_t i = 0; i < arraySize; ++i) {
+        processVector[i] = this->value(i);
+      }
+      _processArray->write();
+    }
+  };
 
 } // namespace ChimeraTK
 
--- include/PropertyDescription.h
+++ include/PropertyDescription.h
@@ -6,176 +6,147 @@
 //#include <set>
 //#include <memory>
 //#include <iostream>
-//#include <ChimeraTK/RegisterPath.h>
+#include <ChimeraTK/RegisterPath.h>
 
 ///@todo FIXME: Separate these out into individual files
 
 namespace ChimeraTK {
 
-// PropertyAttributes are used in the property description itself, and
-// as default values (global and in the locations)
-struct PropertyAttributes {
-  bool hasHistory;
-  bool isWriteable;
-  bool publishZMQ;
-  std::string macroPulseNumberSource;
-  PropertyAttributes(bool hasHistory_ = true, bool isWriteable_ = true,
-                     bool publishZMQ_ = false,
-                     std::string macroPulseNumberSource_ = "")
-      : hasHistory(hasHistory_), isWriteable(isWriteable_),
-        publishZMQ(publishZMQ_),
-        macroPulseNumberSource(macroPulseNumberSource_) {}
-  bool operator==(PropertyAttributes const &other) const {
-    return (hasHistory == other.hasHistory &&
-            isWriteable == other.isWriteable &&
-            publishZMQ == other.publishZMQ &&
-            macroPulseNumberSource == other.macroPulseNumberSource);
-  }
-};
-
-/********************************************************************************************************************/
-
-// Common for all properties, the base class to be stored.
-// FIXME: should sort by name to put it into a set?
-struct PropertyDescription {
-  std::string location;
-  std::string name;
-  PropertyDescription(std::string location_ = "", std::string name_ = "")
-      : location(location_), name(name_) {}
-  virtual bool operator==(PropertyDescription const &other) const {
-    return location == other.location && name == other.name;
-  }
-  virtual const std::type_info &type() const {
-    return typeid(PropertyDescription);
-  }
-  virtual void print(std::ostream &os = std::cout) const {
-    os << location << " / " << name << std::endl;
-  }
-};
-
-/********************************************************************************************************************/
-
-// Combines property attributes and the base description
-// FIXME: should sort by name to put it into a set?
-struct AutoPropertyDescription : public PropertyDescription,
-                                 public PropertyAttributes {
-  ChimeraTK::RegisterPath source;
-  AutoPropertyDescription(ChimeraTK::RegisterPath const &source_ = "",
-                          std::string location_ = "", std::string name_ = "",
-                          bool hasHistory_ = true, bool isWriteable_ = true)
-      : PropertyDescription(location_, name_),
-        PropertyAttributes(hasHistory_, isWriteable_), source(source_) {}
-  virtual bool operator==(PropertyDescription const &other) const override {
-    if (other.type() == typeid(AutoPropertyDescription)) {
-      auto casted_other = static_cast<AutoPropertyDescription const &>(other);
-      return source == casted_other.source && location == other.location &&
-             name == other.name &&
-             static_cast<const PropertyAttributes *>(this)->operator==(
-                 casted_other);
-    } else {
-      return false;
+  // PropertyAttributes are used in the property description itself, and
+  // as default values (global and in the locations)
+  struct PropertyAttributes {
+    bool hasHistory;
+    bool isWriteable;
+    bool publishZMQ;
+    std::string macroPulseNumberSource;
+    PropertyAttributes(bool hasHistory_ = true, bool isWriteable_ = true, bool publishZMQ_ = false,
+        std::string macroPulseNumberSource_ = "")
+    : hasHistory(hasHistory_), isWriteable(isWriteable_), publishZMQ(publishZMQ_),
+      macroPulseNumberSource(macroPulseNumberSource_) {}
+    bool operator==(PropertyAttributes const& other) const {
+      return (hasHistory == other.hasHistory && isWriteable == other.isWriteable && publishZMQ == other.publishZMQ &&
+          macroPulseNumberSource == other.macroPulseNumberSource);
     }
-  }
-  virtual const std::type_info &type() const override {
-    return typeid(AutoPropertyDescription);
-  }
-  virtual void print(std::ostream &os = std::cout) const {
-    os << source << " -> " << location << " / " << name << std::endl;
-  }
-};
+  };
 
-/********************************************************************************************************************/
+  /********************************************************************************************************************/
 
-struct ArrayDescription : public AutoPropertyDescription {
-  enum class DataType { Byte, Short, Int, Long, Float, Double, Auto };
-
-  ArrayDescription(ChimeraTK::RegisterPath const &source_ = "",
-                   std::string location_ = "", std::string name_ = "",
-                   DataType dataType_ = DataType::Auto, bool hasHistory_ = true,
-                   bool isWriteable_ = true)
-      : AutoPropertyDescription(source_, location_, name_, hasHistory_,
-                                isWriteable_),
-        dataType(dataType_) {}
-
-  /// Convenience constructor from an auto property description, just add the
-  /// type
-  ArrayDescription(AutoPropertyDescription const &autoDescription,
-                   DataType dataType_)
-      : AutoPropertyDescription(autoDescription), dataType(dataType_) {}
-
-  virtual bool operator==(PropertyDescription const &other) const override {
-    if (other.type() == typeid(ArrayDescription)) {
-      auto casted_other = static_cast<ArrayDescription const &>(other);
-      return dataType == casted_other.dataType &&
-             static_cast<const AutoPropertyDescription *>(this)->operator==(
-                 casted_other);
-    } else {
-      return false;
+  // Common for all properties, the base class to be stored.
+  // FIXME: should sort by name to put it into a set?
+  struct PropertyDescription {
+    std::string location;
+    std::string name;
+    PropertyDescription(std::string location_ = "", std::string name_ = "") : location(location_), name(name_) {}
+    virtual bool operator==(PropertyDescription const& other) const {
+      return location == other.location && name == other.name;
     }
-  }
+    virtual const std::type_info& type() const { return typeid(PropertyDescription); }
+    virtual void print(std::ostream& os = std::cout) const { os << location << " / " << name << std::endl; }
+  };
 
-  virtual const std::type_info &type() const {
-    return typeid(ArrayDescription);
-  }
-  DataType dataType;
-};
+  /********************************************************************************************************************/
 
-/********************************************************************************************************************/
+  // Combines property attributes and the base description
+  // FIXME: should sort by name to put it into a set?
+  struct AutoPropertyDescription : public PropertyDescription, public PropertyAttributes {
+    ChimeraTK::RegisterPath source;
+    AutoPropertyDescription(ChimeraTK::RegisterPath const& source_ = "", std::string location_ = "",
+        std::string name_ = "", bool hasHistory_ = true, bool isWriteable_ = true)
+    : PropertyDescription(location_, name_), PropertyAttributes(hasHistory_, isWriteable_), source(source_) {}
+    virtual bool operator==(PropertyDescription const& other) const override {
+      if(other.type() == typeid(AutoPropertyDescription)) {
+        auto casted_other = static_cast<AutoPropertyDescription const&>(other);
+        return source == casted_other.source && location == other.location && name == other.name &&
+            static_cast<const PropertyAttributes*>(this)->operator==(casted_other);
+      }
+      else {
+        return false;
+      }
+    }
+    virtual const std::type_info& type() const override { return typeid(AutoPropertyDescription); }
+    virtual void print(std::ostream& os = std::cout) const {
+      os << source << " -> " << location << " / " << name << std::endl;
+    }
+  };
 
-struct SpectrumDescription : public PropertyDescription,
-                             public PropertyAttributes {
-  ChimeraTK::RegisterPath source;
-  ChimeraTK::RegisterPath startSource;
-  ChimeraTK::RegisterPath incrementSource;
-  float start;
-  float increment;
+  /********************************************************************************************************************/
 
-  SpectrumDescription(ChimeraTK::RegisterPath const &source_ = "",
-                      std::string location_ = "", std::string name_ = "",
-                      bool hasHistory_ = true, bool isWriteable_ = true)
-      : PropertyDescription(location_, name_),
-        PropertyAttributes(hasHistory_, isWriteable_), source(source_),
-        start(0), increment(1.0) {}
+  struct ArrayDescription : public AutoPropertyDescription {
+    enum class DataType { Byte, Short, Int, Long, Float, Double, Auto };
 
-  virtual const std::type_info &type() const {
-    return typeid(SpectrumDescription);
-  }
-  virtual void print(std::ostream &os = std::cout) const {
-    os << source << " -> " << location << " / " << name
-       << " (startSource = " << startSource
-       << ", incrementSource = " << incrementSource << ")" << std::endl;
-  }
-};
+    ArrayDescription(ChimeraTK::RegisterPath const& source_ = "", std::string location_ = "", std::string name_ = "",
+        DataType dataType_ = DataType::Auto, bool hasHistory_ = true, bool isWriteable_ = true)
+    : AutoPropertyDescription(source_, location_, name_, hasHistory_, isWriteable_), dataType(dataType_) {}
 
-//    struct SpectrumDescription:
-//      public PropertyDescription, public PropertyAttributes{
-//      float xStart;
-//      float xIncrement;
-//      ChimeraTK::RegisterPath mainSource;
-//      ChimeraTK::RegisterPath xStartSource;
-//      ChimeraTK::RegisterPath xIncrementSource;
-//      SpectrumDescription(float start_=0.0, float increment_=1.0)
-//      : start(start_), increment(increment_){}
-//      bool operator==(SpectrumDescription const & other) const{
-//        return start==other.start && increment==other.increment;
-//      }
-//    };
+    /// Convenience constructor from an auto property description, just add the
+    /// type
+    ArrayDescription(AutoPropertyDescription const& autoDescription, DataType dataType_)
+    : AutoPropertyDescription(autoDescription), dataType(dataType_) {}
 
-/********************************************************************************************************************/
+    virtual bool operator==(PropertyDescription const& other) const override {
+      if(other.type() == typeid(ArrayDescription)) {
+        auto casted_other = static_cast<ArrayDescription const&>(other);
+        return dataType == casted_other.dataType &&
+            static_cast<const AutoPropertyDescription*>(this)->operator==(casted_other);
+      }
+      else {
+        return false;
+      }
+    }
 
-// This is the per location information which are used as default for the
-// properties in this location
-struct LocationInfo : public PropertyAttributes {
-  bool useHasHistoryDefault;
-  bool useIsWriteableDefault;
-  bool useMacroPulseNumberSourceDefault;
-  LocationInfo(bool useHasHistoryDefault_ = false,
-               bool useIsWriteableDefault_ = false,
-               bool useMacroPulseNumberSourceDefault_ = false)
-      : useHasHistoryDefault(useHasHistoryDefault_),
-        useIsWriteableDefault(useIsWriteableDefault_),
-        useMacroPulseNumberSourceDefault(useMacroPulseNumberSourceDefault_) {}
-};
+    virtual const std::type_info& type() const { return typeid(ArrayDescription); }
+    DataType dataType;
+  };
+
+  /********************************************************************************************************************/
+
+  struct SpectrumDescription : public PropertyDescription, public PropertyAttributes {
+    ChimeraTK::RegisterPath source;
+    ChimeraTK::RegisterPath startSource;
+    ChimeraTK::RegisterPath incrementSource;
+    float start;
+    float increment;
+
+    SpectrumDescription(ChimeraTK::RegisterPath const& source_ = "", std::string location_ = "", std::string name_ = "",
+        bool hasHistory_ = true, bool isWriteable_ = true)
+    : PropertyDescription(location_, name_), PropertyAttributes(hasHistory_, isWriteable_), source(source_), start(0),
+      increment(1.0) {}
+
+    virtual const std::type_info& type() const { return typeid(SpectrumDescription); }
+    virtual void print(std::ostream& os = std::cout) const {
+      os << source << " -> " << location << " / " << name << " (startSource = " << startSource
+         << ", incrementSource = " << incrementSource << ")" << std::endl;
+    }
+  };
+
+  //    struct SpectrumDescription:
+  //      public PropertyDescription, public PropertyAttributes{
+  //      float xStart;
+  //      float xIncrement;
+  //      ChimeraTK::RegisterPath mainSource;
+  //      ChimeraTK::RegisterPath xStartSource;
+  //      ChimeraTK::RegisterPath xIncrementSource;
+  //      SpectrumDescription(float start_=0.0, float increment_=1.0)
+  //      : start(start_), increment(increment_){}
+  //      bool operator==(SpectrumDescription const & other) const{
+  //        return start==other.start && increment==other.increment;
+  //      }
+  //    };
+
+  /********************************************************************************************************************/
+
+  // This is the per location information which are used as default for the
+  // properties in this location
+  struct LocationInfo : public PropertyAttributes {
+    bool useHasHistoryDefault;
+    bool useIsWriteableDefault;
+    bool useMacroPulseNumberSourceDefault;
+    LocationInfo(bool useHasHistoryDefault_ = false,
+        bool useIsWriteableDefault_ = false,
+        bool useMacroPulseNumberSourceDefault_ = false)
+    : useHasHistoryDefault(useHasHistoryDefault_), useIsWriteableDefault(useIsWriteableDefault_),
+      useMacroPulseNumberSourceDefault(useMacroPulseNumberSourceDefault_) {}
+  };
 
 } // namespace ChimeraTK
 
