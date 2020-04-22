3bb139010f1cdbb4209c3774872209ddcab4b907
Martin Hierholzer <martin.hierholzer@desy.de>
2020-04-22 15:33:23 +0200

    do not use deprecated DOOCS API and fix some warnings

--- include/D_textUnifier.h
+++ include/D_textUnifier.h
@@ -2,7 +2,6 @@
 #define D_TEXTUNIFIER_H
 
 #include <d_fct.h>
-#include <eq_sts_codes.h>
 
 /** A compatibility class that adds the constructor which would create a history,
  *  and the set_and_archive function to the D_text interface. As this does not
@@ -14,9 +13,8 @@ struct D_textUnifier : public D_text {
   using D_text::D_text;
   D_textUnifier(EqFct* eqFct, std::string doocsPropertyName);
 
-  /// \todo FIXE: eq_sts_codes will be deprecated, but currently is require to compile with the latest pubilshed doocs release
-  /// It will be changed to ArchiveStatus.
-  void set_and_archive(const std::string& str, eq_sts_codes status = sts_ok, u_int seconds = 0, u_int microseconds = 0);
+  void set_and_archive(
+      const std::string& str, ArchiveStatus status, doocs::Timestamp timestamp, doocs::EventId eventId);
   D_hist* get_histPointer();
 };
 
