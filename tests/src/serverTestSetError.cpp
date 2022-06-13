1aa892f5f6c4a80c15a3ae82f7aca544c8437588
Dietrich Rothe <dietrich.rothe@desy.de>
2022-06-13 15:23:46 +0200

    add basic test for set_error tag

---
+++ tests/src/serverTestSetError.cpp
@@ -0,0 +1,50 @@
+#define BOOST_TEST_MODULE serverTestSetError
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <ChimeraTK/ControlSystemAdapter/Testing/ReferenceTestApplication.h>
+
+extern const char* object_name;
+#include <doocs-server-test-helper/ThreadedDoocsServer.h>
+#include <doocs-server-test-helper/doocsServerTestHelper.h>
+
+#include <eq_fct.h>
+
+#include "DoocsAdapter.h"
+#include "serverBasedTestTools.h"
+
+using namespace boost::unit_test_framework;
+using namespace ChimeraTK;
+
+DOOCS_ADAPTER_DEFAULT_FIXTURE_STATIC_APPLICATION
+
+std::string lastErrString = "(unset)";
+static eq_errors checkLocationError(const std::string& property) {
+  auto location = getLocationFromPropertyAddress(property);
+  location->lock();
+  int code = location->get_error();
+  lastErrString = location->get_errorstr();
+  location->unlock();
+
+  return static_cast<eq_errors>(code);
+}
+
+// basic check for error code handling, without associated message source
+BOOST_AUTO_TEST_CASE(testErrorNoMessageSource) {
+  auto check = std::bind(checkLocationError, "//INT/FROM_DEVICE_SCALAR");
+  // Testing initial state
+  GlobalFixture::referenceTestApplication.dataValidity = DataValidity::ok;
+  DoocsServerTestHelper::doocsSet<int>("//INT/TO_DEVICE_SCALAR", 0);
+
+  GlobalFixture::referenceTestApplication.runMainLoopOnce();
+  checkWithTimeout<eq_errors>(check, no_error);
+  BOOST_CHECK(lastErrString == "ok");
+
+  // Check fault state
+  GlobalFixture::referenceTestApplication.dataValidity = DataValidity::faulty;
+  DoocsServerTestHelper::doocsSet<int>("//INT/TO_DEVICE_SCALAR", 1);
+  GlobalFixture::referenceTestApplication.runMainLoopOnce();
+  checkWithTimeout<eq_errors>(check, not_available);
+  // there must be a generic error message
+  BOOST_CHECK(lastErrString != "ok");
+}
