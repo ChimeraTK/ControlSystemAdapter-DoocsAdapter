9d7b02bc3008c7ffd35cca30eea1c4b6e70a29da
Dietrich Rothe <dietrich.rothe@desy.de>
2022-11-14 18:56:14 +0100

    get rid of compiler warning
    which was due to overly strict check of definition=declaration

--- include/DoocsProcessScalar.h
+++ include/DoocsProcessScalar.h
@@ -51,14 +51,14 @@ namespace ChimeraTK {
 
   template<typename T, typename DOOCS_T>
   DoocsProcessScalar<T, DOOCS_T>::DoocsProcessScalar(EqFct* eqFct, std::string doocsPropertyName,
-      const boost::shared_ptr<ChimeraTK::NDRegisterAccessor<T>>& processScalar, DoocsUpdater& updater)
+      boost::shared_ptr<typename ChimeraTK::NDRegisterAccessor<T>> const& processScalar, DoocsUpdater& updater)
   : DOOCS_T(eqFct, doocsPropertyName.c_str()), PropertyBase(doocsPropertyName, updater), _processScalar(processScalar) {
     setupOutputVar(processScalar);
   }
 
   template<typename T, typename DOOCS_T>
   DoocsProcessScalar<T, DOOCS_T>::DoocsProcessScalar(std::string doocsPropertyName, EqFct* eqFct,
-      const boost::shared_ptr<ChimeraTK::NDRegisterAccessor<T>>& processScalar, DoocsUpdater& updater)
+      boost::shared_ptr<typename ChimeraTK::NDRegisterAccessor<T>> const& processScalar, DoocsUpdater& updater)
   : DOOCS_T(doocsPropertyName.c_str(), eqFct), PropertyBase(doocsPropertyName, updater), _processScalar(processScalar) {
     setupOutputVar(processScalar);
   }
