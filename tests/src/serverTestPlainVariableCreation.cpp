75d19d9c9d963ceb2137be1224dab0c5256c1326
Jens Georg <jens.georg@desy.de>
2024-04-09 15:26:04 +0200

    fix: Stray whitespace in test

--- tests/src/serverTestPlainVariableCreation.cpp
+++ tests/src/serverTestPlainVariableCreation.cpp
@@ -23,7 +23,7 @@ DOOCS_ADAPTER_DEFAULT_FIXTURE
 /// Check that all expected variables are there.
 BOOST_AUTO_TEST_CASE(testVariableExistence) {
   for(auto const location : {"CHAR", "DOUBLE", "FLOAT", "INT", "SHORT", "UCHAR", "UINT", "USHORT"}) {
-    for(auto const property : {"CONSTANT_ARRAY", "FROM_DEVICE_ARRAY", "TO_DEVICE_ARRAY "}) {
+    for(auto const property : {"CONSTANT_ARRAY", "FROM_DEVICE_ARRAY", "TO_DEVICE_ARRAY"}) {
       // if this throws the property does not exist. we should always be able to
       // read"
       BOOST_CHECK_NO_THROW(
