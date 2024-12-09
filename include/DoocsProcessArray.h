ChimeraTK/ControlSystemAdapter-DoocsAdapter

commit 1992ded514439a5e6fb2ab933ec33cb8e8eaf7ee

Feature: synchronise multi-mapped writeable variables
If a writeable process variable is mapped to multiple properties, so far
the values in the properties were not synchronised, i.e. writing to one
property did not propagate the value to the other connected properties.
This synchronisation is now implemented.

Note that inconsistencies might occur when more than one of the
properties is writeable, hence a warning will be displayed in this case.

include/DoocsProcessArray.h
@@ -17,7 +17,7 @@
 namespace ChimeraTK {
 
   template<typename DOOCS_T, typename DOOCS_PRIMITIVE_T>
-  class DoocsProcessArray : public DOOCS_T, public boost::noncopyable {
+  class DoocsProcessArray : public DOOCS_T, public boost::noncopyable, public PropertyBase {
    public:
     DoocsProcessArray(EqFct* eqFct, std::string const& doocsPropertyName,
         boost::shared_ptr<ChimeraTK::NDRegisterAccessor<DOOCS_PRIMITIVE_T>> const& processArray, DoocsUpdater& updater)
@@ -54,7 +54,7 @@ namespace ChimeraTK {
     void set(EqAdr* eqAdr, EqData* data1, EqData* data2, EqFct* eqFct) override {
       DOOCS_T::set(eqAdr, data1, data2, eqFct);
       modified = true;
-      sendToDevice();
+      sendToDevice(true);
       // send data via ZeroMQ if enabled and if DOOCS initialisation is complete
       if(publishZMQ && ChimeraTK::DoocsAdapter::isInitialised) {
         auto timestamp = _processArray->getVersionNumber().getTime();
@@ -79,23 +79,27 @@ namespace ChimeraTK {
       }
     }
 
+    EqFct* getEqFct() override { return this->get_eqfct(); }
+
     /**
      * Override the Doocs auto_init() method, which is called after initialising
      * the value of the property from the config file.
      */
     void auto_init(void) override {
+      doocsAdapter.before_auto_init();
+
       DOOCS_T::auto_init();
       modified = false;
       // send the current value to the device
-      if(_processArray->isWriteable()) {
-        sendToDevice();
+      if(this->get_access() == 1) { // property is writeable
+        sendToDevice(false);
         // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,
         // which breaks consistency check in ZeroMQ subscriptions after the 4 minutes timeout)
         DOOCS_T::set_stamp();
       }
     }
 
-    void updateDoocsBuffer(TransferElementID transferElementId) {
+    void updateDoocsBuffer(const TransferElementID& transferElementId) override {
       // FIXME: A first  implementation is checking the data consistency here. Later this should be
       // before calling this function because calling this function through a function pointer is
       // comparatively expensive.
@@ -224,7 +228,7 @@ namespace ChimeraTK {
     // Internal function which copies the content from the DOOCS container into
     // the ChimeraTK ProcessArray and calls the send method. Factored out to allow
     // unit testing.
-    void sendToDevice() {
+    void sendToDevice(bool getLocks) {
       // Brute force implementation with a loop. Works for all data types.
       // always get a fresh reference
       auto& processVector = _processArray->accessChannel(0);
@@ -233,6 +237,15 @@ namespace ChimeraTK {
         processVector[i] = this->value(i);
       }
       _processArray->write();
+
+      // make sure other properties using these PVs see the update
+      if(getLocks) this->get_eqfct()->unlock();
+      for(auto& prop : otherPropertiesToUpdate) {
+        if(getLocks) prop->getEqFct()->lock();
+        prop->updateDoocsBuffer({});
+        if(getLocks) prop->getEqFct()->unlock();
+      }
+      if(getLocks) this->get_eqfct()->lock();
     }
   };
 

include/DoocsProcessScalar.h
@@ -18,9 +18,9 @@ namespace ChimeraTK {
    *  \li \c DOOCS_T, The Doocs type which is used
    */
   template<typename T, typename DOOCS_T>
-  class DoocsProcessScalar : public DOOCS_T, public boost::noncopyable {
+  class DoocsProcessScalar : public DOOCS_T, public boost::noncopyable, public PropertyBase {
    public:
-    void updateDoocsBuffer(TransferElementID transferElementId) {
+    void updateDoocsBuffer(const TransferElementID& transferElementId) override {
       // FIXME: A first  implementation is checking the data consistency here. Later this should be
       // before calling this function because calling this function through a function pointer is
       // comparatively expensive.
@@ -96,6 +96,8 @@ namespace ChimeraTK {
       }
     }
 
+    EqFct* getEqFct() override { return this->get_eqfct(); }
+
     DoocsProcessScalar(EqFct* eqFct, std::string doocsPropertyName,
         boost::shared_ptr<typename ChimeraTK::NDRegisterAccessor<T>> const& processScalar, DoocsUpdater& updater)
     : DOOCS_T(eqFct, doocsPropertyName.c_str()), _processScalar(processScalar), _doocsUpdater(updater), _eqFct(eqFct) {
@@ -143,6 +145,16 @@ namespace ChimeraTK {
       else {
         throw ChimeraTK::logic_error("Trying to write to a non-writable variable");
       }
+
+      // make sure other properties using these PVs see the update
+      this->get_eqfct()->unlock();
+      for(auto& prop : otherPropertiesToUpdate) {
+        prop->getEqFct()->lock();
+        prop->updateDoocsBuffer({});
+        prop->getEqFct()->unlock();
+      }
+      this->get_eqfct()->lock();
+
       // send data via ZeroMQ if enabled and if DOOCS initialisation is complete
       if(_publishZMQ && ChimeraTK::DoocsAdapter::isInitialised) {
         auto timestamp = _processScalar->getVersionNumber().getTime();
@@ -173,14 +185,21 @@ namespace ChimeraTK {
      * the value of the property from the config file.
      */
     void auto_init(void) override {
+      doocsAdapter.before_auto_init();
+
       DOOCS_T::auto_init();
       // send the current value to the device
-      if(_processScalar->isWriteable()) {
+      if(this->get_access() == 1) { // property is writeable
         _processScalar->accessData(0) = DOOCS_T::value();
         _processScalar->write();
         // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,
         // which breaks consistency check in ZeroMQ subscriptions after the 4 minutes timeout)
         DOOCS_T::set_stamp();
+
+        // make sure other properties using these PVs see the update
+        for(auto& prop : otherPropertiesToUpdate) {
+          prop->updateDoocsBuffer({});
+        }
       }
     }
 

src/DoocsSpectrum.cc
@@ -61,7 +61,7 @@ namespace ChimeraTK {
   void DoocsSpectrum::set(EqAdr* eqAdr, EqData* data1, EqData* data2, EqFct* eqFct) {
     D_spectrum::set(eqAdr, data1, data2, eqFct);
     modified = true;
-    sendToDevice();
+    sendToDevice(true);
 
     // send data via ZeroMQ if enabled and if DOOCS initialisation is complete
     if(publishZMQ && ChimeraTK::DoocsAdapter::isInitialised) {
@@ -88,6 +88,8 @@ namespace ChimeraTK {
   }
 
   void DoocsSpectrum::auto_init(void) {
+    doocsAdapter.before_auto_init();
+
     // check if the macro pulse number source has been set if the spectrum is buffered
     if(nBuffers > 1) {
       if(_macroPulseNumberSource == nullptr) {
@@ -99,8 +101,8 @@ namespace ChimeraTK {
     // send the current value to the device
     D_spectrum::read();
     modified = false;
-    if(_processArray->isWriteable()) {
-      sendToDevice();
+    if(this->get_access() == 1) { // property is writeable
+      sendToDevice(false);
       // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,
       // which breaks consistency check in ZeroMQ subscriptions after the 4 minutes timeout)
       D_spectrum::set_stamp();
@@ -120,7 +122,7 @@ namespace ChimeraTK {
     D_spectrum::write(s);
   }
 
-  void DoocsSpectrum::updateDoocsBuffer(TransferElementID transferElementId) {
+  void DoocsSpectrum::updateDoocsBuffer(const TransferElementID& transferElementId) {
     // Note: we already own the location lock by specification of the DoocsUpdater
 
     // FIXME: A first  implementation is checking the data consistency here. Later this should be
@@ -252,7 +254,7 @@ namespace ChimeraTK {
     }
   }
 
-  void DoocsSpectrum::sendToDevice() {
+  void DoocsSpectrum::sendToDevice(bool getLock) {
     // Brute force implementation with a loop. Works for all data types.
     // FIXME: find the efficient, memcopying function for float
     // always get a fresh reference
@@ -262,6 +264,15 @@ namespace ChimeraTK {
       processVector[i] = read_spectrum(i);
     }
     _processArray->write();
+
+    // make sure other properties using these PVs see the update
+    if(getLock) this->get_eqfct()->unlock();
+    for(auto& prop : otherPropertiesToUpdate) {
+      if(getLock) prop->getEqFct()->lock();
+      prop->updateDoocsBuffer({});
+      if(getLock) prop->getEqFct()->unlock();
+    }
+    if(getLock) this->get_eqfct()->lock();
   }
 
 } // namespace ChimeraTK

include/DoocsAdapter.h
@@ -2,10 +2,26 @@
 #define _CHIMERATK_DOOCS_ADAPTER_H_
 
 #include "CSAdapterEqFct.h"
-#include "DoocsAdapter.h"
 #include "DoocsPVFactory.h"
 
 namespace ChimeraTK {
+
+  /**
+   * Base class used for all properties.
+   */
+  class PropertyBase : public boost::enable_shared_from_this<PropertyBase> {
+   public:
+    // List of other properties which need to update their DOOCS buffers when this property is written from the DOOCS
+    // side. This is used to synchronise multi-mapped PVs.
+    std::set<boost::shared_ptr<PropertyBase>> otherPropertiesToUpdate;
+
+    // Update DOOCS buffer from PVs. The given transferElementId shall be used only for checking consistency with the
+    // DataConsistencyGroup. {} will be passed if the update is coming from another property (hence the update will
+    // only be taken with data matching set to none, which is the expected behaviour).
+    virtual void updateDoocsBuffer(const TransferElementID& transferElementId) = 0;
+
+    virtual EqFct* getEqFct() = 0;
+  };
 
   /** The main adapter class. With this tool the EqFct should shrink to about 4
    * lines of code (plus boiler plate).
@@ -18,6 +34,12 @@ namespace ChimeraTK {
 
     boost::shared_ptr<DoocsUpdater> updater;
 
+    // Function to be called in all auto_init() implementations, to initialise otherPropertiesToUpdate lists in all
+    // properties. This needs to be done after all locations have been created but before the properties get their
+    // initial values from the config file. DOOCS seems not to provide any hook at that point... This function will only
+    // perform an action when called for the first time.
+    void before_auto_init();
+
     // An atomic bool which is set true in post_init_epilog to indicate that doocs
     // is ready. Only used in testing.
     static std::atomic<bool> isInitialised;
@@ -28,11 +50,20 @@ namespace ChimeraTK {
     // Function used by the property implementations to decide whether to print a "data loss" warning
     static bool checkPrintDataLossWarning(size_t counter);
 
+    // stores list of writable PVs which are mapped to multiple properties
+    // Note: this is cleared in post_init_epilog() to save memory.
+    std::map<std::string, std::set<boost::shared_ptr<PropertyBase>>> writeableVariablesWithMultipleProperties;
+
    protected:
     boost::shared_ptr<ControlSystemPVManager> _controlSystemPVManager;
     boost::shared_ptr<DevicePVManager> _devicePVManager;
+
+    // flag whether before_auto_init() has already been called.
+    bool before_auto_init_called{false};
   };
 
 } // namespace ChimeraTK
+
+extern ChimeraTK::DoocsAdapter doocsAdapter;
 
 #endif // _CHIMERATK_DOOCS_ADAPTER_H_

include/DoocsIfff.h
@@ -6,12 +6,14 @@
 #include <ChimeraTK/NDRegisterAccessor.h>
 #include <ChimeraTK/DataConsistencyGroup.h>
 
+#include "DoocsAdapter.h"
+
 class EqFct;
 
 namespace ChimeraTK {
   class DoocsUpdater;
 
-  class DoocsIfff : public D_ifff, public boost::noncopyable {
+  class DoocsIfff : public D_ifff, public boost::noncopyable, public PropertyBase {
    public:
     // Constructor with history enabled
     DoocsIfff(EqFct* eqFct, std::string const& doocsPropertyName,
@@ -35,11 +37,13 @@ namespace ChimeraTK {
     DataConsistencyGroup _consistencyGroup;
     
    protected:
-    void updateAppToDoocs(TransferElementID& elementId);
-    void sendToApplication();
+    void updateDoocsBuffer(const TransferElementID& elementId) override;
+    void sendToApplication(bool getLock);
     void registerVariable(const ChimeraTK::TransferElementAbstractor& var);
     void registerIfffSources();
     void checkSourceConsistency();
+
+    EqFct* getEqFct() override { return this->get_eqfct(); }
 
     boost::shared_ptr<NDRegisterAccessor<int>> _i1Value;
     boost::shared_ptr<NDRegisterAccessor<float>> _f1Value;

include/DoocsSpectrum.h
@@ -4,6 +4,7 @@
 #include <D_spectrum.h>
 #include <boost/noncopyable.hpp>
 
+#include "DoocsAdapter.h"
 #include "DoocsUpdater.h"
 #include "splitStringAtFirstSlash.h"
 #include <ChimeraTK/NDRegisterAccessor.h>
@@ -17,7 +18,7 @@ class EqFct;
 
 namespace ChimeraTK {
 
-  class DoocsSpectrum : public D_spectrum, public boost::noncopyable {
+  class DoocsSpectrum : public D_spectrum, public boost::noncopyable, public PropertyBase {
    public:
     /** The constructor expects an NDRegisterAccessor of float, which usually will
      * be a decorator to the implementation type. The decorator cannot be
@@ -64,7 +65,9 @@ namespace ChimeraTK {
     void auto_init(void) override;
 
     // call this function after a tranfer element has requested it.
-    void updateDoocsBuffer(TransferElementID transferElementId);
+    void updateDoocsBuffer(const TransferElementID& transferElementId) override;
+
+    EqFct* getEqFct() override { return this->get_eqfct(); }
 
     // callback function after the start or increment variables have changed
     void updateParameters();
@@ -88,7 +91,7 @@ namespace ChimeraTK {
     // Internal function which copies the content from the DOOCS container into
     // the ChimeraTK ProcessArray and calls the send method. Factored out to allow
     // unit testing.
-    void sendToDevice();
+    void sendToDevice(bool getLock);
 
     // Flag whether the value has been modified since the content has been saved to disk the last time (see write()).
     bool modified{false};

include/DoocsXY.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "DoocsAdapter.h"
+
 #include <D_xy.h>
 #include <boost/noncopyable.hpp>
 
@@ -11,14 +13,15 @@ class EqFct;
 namespace ChimeraTK {
   class DoocsUpdater;
 
-  class DoocsXy : public D_xy, public boost::noncopyable {
+  class DoocsXy : public D_xy, public boost::noncopyable, public PropertyBase {
    public:
     DoocsXy(EqFct* eqFct, std::string const& doocsPropertyName,
         boost::shared_ptr<NDRegisterAccessor<float>> const& xValues,
         boost::shared_ptr<NDRegisterAccessor<float>> const& yValues, DoocsUpdater& updater);
 
    protected:
-    void updateValues(TransferElementID& elementId);
+    void updateDoocsBuffer(const TransferElementID& elementId) override;
+    EqFct* getEqFct() override { return this->get_eqfct(); }
 
     DataConsistencyGroup _consistencyGroup;
     boost::shared_ptr<NDRegisterAccessor<float>> _xValues;

include/PropertyDescription.h
@@ -78,6 +78,7 @@ namespace ChimeraTK {
     }
     virtual const std::type_info& type() const { return typeid(PropertyDescription); }
     virtual void print(std::ostream& os = std::cout) const { os << location << " / " << name << std::endl; }
+    virtual std::set<std::string> getSources() = 0;
   };
 
   /********************************************************************************************************************/
@@ -117,6 +118,8 @@ namespace ChimeraTK {
       if(info == typeid(ChimeraTK::Void)) dataType = DataType::Void;
     }
 
+    std::set<std::string> getSources() override { return {source}; };
+
     DataType dataType;
   };
 
@@ -149,6 +152,13 @@ namespace ChimeraTK {
       os << source << " -> " << location << " / " << name << " (startSource = " << startSource
          << ", incrementSource = " << incrementSource << ", numberOfBuffers = " << numberOfBuffers << ")" << std::endl;
     }
+
+    std::set<std::string> getSources() override {
+      std::set<std::string> ret{source};
+      if(startSource.length() > 1) ret.insert(startSource);
+      if(incrementSource.length() > 1) ret.insert(incrementSource);
+      return ret;
+    };
   };
 
   /********************************************************************************************************************/
@@ -176,6 +186,8 @@ namespace ChimeraTK {
     void print(std::ostream& os = std::cout) const override {
       os << "x: " << xSource << " y: " << ySource << " -> " << location << " / " << name << std::endl;
     }
+
+    std::set<std::string> getSources() override { return {xSource, ySource}; };
   };
 
   /********************************************************************************************************************/
@@ -196,6 +208,8 @@ namespace ChimeraTK {
       os << "i1: " << i1Source << ", f1: " << f1Source << ", f2: " << f2Source << ", f3: " << f3Source << " -> "
          << location << " / " << name << std::endl;
     }
+
+    std::set<std::string> getSources() override { return {i1Source, f1Source, f2Source, f3Source}; };
   };
 
   /********************************************************************************************************************/

commit f7437892377f076b927328353d9d210f46333a21

fix: Propagate initial value for r/o WB variables
If you had a PushInputWB mapped to DOOCS but changed it not to be
writeable, you would not receive a proper initial value but 0.

Now, if there is only one variable connected to this, write down the
initial value nevertheless.

Issue was introduced with 1992ded514439a5e6fb2ab933ec33cb8e8eaf7ee

include/DoocsProcessArray.h
@@ -91,7 +91,12 @@ namespace ChimeraTK {
     DOOCS_T::auto_init();
     modified = false;
     // send the current value to the device
-    if(this->get_access() == 1) { // property is writeable
+    // property is writeable OR the target accessor is writable and the only one connected to this property
+    // The second case is to have bi-directional variables that are used to persist settings into the config file
+    // and need that value back on start-up but are not supposed to be written by the control system and mapped
+    // read-only in the configuration file.
+    if(this->get_access() == 1 ||
+        (_processArray->isWriteable() && otherPropertiesToUpdate.empty())) { // property is writeable
       sendToDevice(false);
       // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,
       // which breaks consistency check in ZeroMQ subscriptions after the 4 minutes timeout)

include/DoocsProcessScalar.h
@@ -94,7 +94,11 @@ namespace ChimeraTK {
 
     DOOCS_T::auto_init();
     // send the current value to the device
-    if(this->get_access() == 1) { // property is writeable
+    // property is writeable OR the target accessor is writable and the only one connected to this property
+    // The second case is to have bi-directional variables that are used to persist settings into the config file
+    // and need that value back on start-up but are not supposed to be written by the control system and mapped
+    // read-only in the configuration file.
+    if(this->get_access() == 1 || (_processScalar->isWriteable() && otherPropertiesToUpdate.empty())) {
       _processScalar->accessData(0) = DOOCS_T::value();
       _processScalar->write();
       // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,

src/DoocsSpectrum.cc
@@ -67,7 +67,8 @@ namespace ChimeraTK {
     // send the current value to the device
     D_spectrum::read();
     modified = false;
-    if(this->get_access() == 1) { // property is writeable
+    if(this->get_access() == 1 ||
+        (_processArray->isWriteable() && otherPropertiesToUpdate.empty())) { // property is writeable
       sendToDevice(false);
       // set DOOCS time stamp, workaround for DOOCS bug (get() always gives current time stamp if no timestamp is set,
       // which breaks consistency check in ZeroMQ subscriptions after the 4 minutes timeout)